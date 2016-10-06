8dc409765d54ca57b09f0b7f8b4165ffb40d589f
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2016-10-06 23:50:02 +0900

    add file-error handling

--- clcat/cat.lisp
+++ clcat/cat.lisp
@@ -1,9 +1,13 @@
 #! /usr/bin/sbcl --script
 
-(let ((in (open (car (cdr *posix-argv*)) :if-does-not-exist nil)))
-  (when in
-    (loop for line = (read-line in nil)
-          while line do (format t "~a~%" line))
-    (close in)))
+(handler-case
+ (let ((in (open (car (cdr *posix-argv*)) :if-does-not-exist :error)))
+   (when in
+     (loop for line = (read-line in nil)
+           while line do (format t "~a~%" line))
+     (close in)))
+ ;; file-error.
+ (file-error (c) (format t "~a~%" c)))
 
-(prin1 (car (cdr *posix-argv*)))
+;; print argv 1.
+(format t "~%PATH: ~a~%" (car (cdr *posix-argv*)))
