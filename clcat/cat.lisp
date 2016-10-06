e0166b10a1f15542547941eb15174ec482de3835
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2016-10-06 23:31:14 +0900

    update open file command arg file

--- clcat/cat.lisp
+++ clcat/cat.lisp
@@ -1,7 +1,9 @@
 #! /usr/bin/sbcl --script
 
-(let ((in (open "~/temp/lisp/study/hello.lisp" :if-does-not-exist nil)))
+(let ((in (open (car (cdr *posix-argv*)) :if-does-not-exist nil)))
   (when in
     (loop for line = (read-line in nil)
           while line do (format t "~a~%" line))
     (close in)))
+
+(prin1 (car (cdr *posix-argv*)))
