fbf6de9a5520bde76766c2cb694c6ec75605671a
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2016-10-07 00:15:57 +0900

    refactoring loop

--- clcat/cat.lisp
+++ clcat/cat.lisp
@@ -1,15 +1,16 @@
 #! /usr/bin/sbcl --script
 
-(defun read-file (f)
-  (loop for x in (cdr f) do
-        (let ((in (open x :if-does-not-exist :error)))
-          (when in
-            (loop for line = (read-line in nil)
-                  while line do (format t "~a~%" line))
-            (close in)))))
+(defun read-file (x)
+  (let ((in (open x :if-does-not-exist :error)))
+    (when in
+      (loop for line = (read-line in nil)
+            while line do (format t "~a~%" line))
+      (close in))))
 
-(handler-case (read-file *posix-argv*)
- ;; file-error.
+(handler-case
+ (loop for x in (cdr *posix-argv*) do
+       (read-file x))
+       ;; file-error.
  (file-error (c) (format t "~a~%" c)))
 
 ;; print argv 1.
