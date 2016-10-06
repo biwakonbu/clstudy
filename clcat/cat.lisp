d9887ea372d89c5e8a07767f773778683d2f06bc
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2016-10-07 00:12:52 +0900

    update concatenate files

--- clcat/cat.lisp
+++ clcat/cat.lisp
@@ -1,15 +1,16 @@
 #! /usr/bin/sbcl --script
 
 (defun read-file (f)
- (let ((in (open (car (cdr f)) :if-does-not-exist :error)))
-   (when in
-     (loop for line = (read-line in nil)
-           while line do (format t "~a~%" line))
-     (close in))))
+  (loop for x in (cdr f) do
+        (let ((in (open x :if-does-not-exist :error)))
+          (when in
+            (loop for line = (read-line in nil)
+                  while line do (format t "~a~%" line))
+            (close in)))))
 
 (handler-case (read-file *posix-argv*)
  ;; file-error.
  (file-error (c) (format t "~a~%" c)))
 
 ;; print argv 1.
-(format t "~%PATH: ~a~%" (car (cdr *posix-argv*)))
+;; (format t "~%PATH: ~a~%" (car (cdr *posix-argv*)))
