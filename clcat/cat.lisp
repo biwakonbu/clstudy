8e0fceff06d96a6975bf2afe64697ef36aad9000
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2016-10-10 21:25:23 +0900

    add squeeze-blank option

--- clcat/cat.lisp
+++ clcat/cat.lisp
@@ -12,14 +12,29 @@
       (format nil "~vT~a~a" (number-of-digits n) n #\TAB)
       ""))
 
+(defun blank-line-p (line)
+  (equal line ""))
+
+(defun not-blank-line-p (line)
+  (not (blank-line-p line)))
+
+(defun squeeze-blank-option-p ()
+    (gethash '-s *option-hash*))
+
 (defun read-file (x)
   (with-open-file (in x :if-does-not-exist :error)
-    (let ((count 1))
+    (let ((count 1) (pre-line-blank-p nil))
       (when in
         (loop for line = (read-line in nil)
            while line do
-             (format t "~a~a~%" (set-line-number count) line)
-             (incf count))))))
+             (when (or (not (squeeze-blank-option-p))
+                       (not-blank-line-p line)
+                       (and (not pre-line-blank-p) (blank-line-p line)))
+               (format t "~a~a~%" (set-line-number count) line)
+               (setf pre-line-blank-p nil)
+               (incf count))
+             (when (and (blank-line-p line) (not pre-line-blank-p))
+               (setf pre-line-blank-p t)))))))
 
 (defun recursive-read (files)
   (if (car files)
